f3756437b54ae9b21b22c6965bee353ce72e55ac
vishal prajapati <163145008+vishalgola@users.noreply.github.com>
2026-04-15 23:55:40 +0530

    Update user_input.py

--- backend/schema/user_input.py
+++ backend/schema/user_input.py
@@ -1,4 +1,6 @@
-
+from pydantic import BaseModel, Field, computed_field, field_validator
+from typing import Literal, Annotated
+from config.city_tier import tier_1_cities, tier_2_cities
 
 
 # pydantic model to validate incoming data
