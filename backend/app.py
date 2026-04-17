330d966e611821770716334e0d338e3538936257
vishal prajapati <163145008+vishalgola@users.noreply.github.com>
2026-04-17 22:20:38 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,8 @@
-
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from schema.user_input import UserInput
+from schema.prediction_response import PredictionResponse
+from model.predict import predict_output, model, MODEL_VERSION
 
 app = FastAPI()
 
